948b79786d9264b6968582d50303b99734b4ffbe
Arsene Awounou <developer.awounou@gmail.com>
2023-04-04 18:46:00 +0100

    make Logo component accept className as props

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,10 @@
+import { LogoProps } from '@/interfaces';
 import Link from 'next/link';
 import * as React from 'react';
 
-function Logo() {
+function Logo({ className='' }: LogoProps) {
   return (
-    <Link href='/'>
+    <Link className={`inline-block ${className}`} href='/'>
       <img width={180} height={50} src="/logo-dark-transparent.png" alt="Spit-it logo" />
     </Link>
   );
