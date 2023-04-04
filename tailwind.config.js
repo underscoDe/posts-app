1c6cfdb9ac55959b3484820399e28f61b2f4fbd6
Arsene Awounou <developer.awounou@gmail.com>
2023-04-04 19:33:39 +0100

    sidebar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'sidebar': 'rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px',
+      }
+    },
   },
   plugins: [],
 };
