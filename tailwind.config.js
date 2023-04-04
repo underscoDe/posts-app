59faece6de6740f8c5ae7c0b4d4f86958be03b95
Arsene Awounou <developer.awounou@gmail.com>
2023-04-04 20:06:05 +0100

    primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: 'hsl(var(--tw-color-primary))',
+      },
       boxShadow: {
         'sidebar': 'rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px',
       }
