ee31a4341e32e66b2a261c43e160a5ccde0468c0
Arsene Awounou <developer.awounou@gmail.com>
2023-04-04 16:34:20 +0100

    create a Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function Logo() {
+  return (
+    <div>
+      <img width={180} height={50} src="/logo-dark-transparent.png" alt="Spit-it logo" />
+    </div>
+  );
+}
+
+export default Logo;
