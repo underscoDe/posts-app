3595311006100db10947714be939cdb94bfbe2c9
Arsene Awounou <developer.awounou@gmail.com>
2023-04-08 12:50:37 +0100

    start implementing login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import Layout from "@/components/Layout";
+import LoginModal from "@/modals/LoginModal/LoginModal";
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
