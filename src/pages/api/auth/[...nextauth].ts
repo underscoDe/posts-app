1fb954255e7990ac49a93ab3918162687bc72543
Arsene Awounou <developer.awounou@gmail.com>
2023-04-09 17:07:51 +0100

    dynamic API route

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,44 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+import prisma from "@/libs/prismadb";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+            throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
